3bc16d68465b1b04f412709f3590dacb19292dd5
kelvin-maker <dennismugaog97@gmail.com>
2021-12-07 17:51:08 -0800

    based on 1-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -9,5 +9,10 @@ class Square():
     def __init__(self, size=0):
         """ Instantiate a 'Square'
         """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
         
+                                                                        
