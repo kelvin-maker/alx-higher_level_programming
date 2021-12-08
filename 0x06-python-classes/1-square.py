14942d64c90402ad95cba230a6fdb39301fa37d1
kelvin-maker <dennismugaog97@gmail.com>
2021-12-07 17:38:42 -0800

    attribute size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Module providing a definition of a class 'Square'
+"""
+
+
+class Square():
+    """ Definition of a 'Square'
+    """
+    def __init__(self, size=0):
+        """ Instantiate a 'Square'
+        """
+        self.__size = size
+        
