7374dcef720810d8a7a3ef2aebe4df9fa2c06e11
kelvin-maker <dennismugaog97@gmail.com>
2021-12-13 10:45:49 -0800

    first commit

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/pyhon3
+
+'''
+An empty Class Defigning a Rectangle
+
+'''
+
+class Rectangle():
+
+    '''
+    Instantiate a Rectangle 
+
+    '''
+    def _init_(self, size=0):
+
+        self.size = size
+               
