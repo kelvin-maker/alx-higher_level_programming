130a8717c5b7021361e208425042877db235c597
kelvin-maker <dennismugaog97@gmail.com>
2021-12-20 15:35:50 -0800

    Write a class Square that inherits from Rectangle (9-rectangle.py

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Provides a class to represent squares
+"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ Definition of fixed-size square
+    """
+    def __init__(self, size):
+        """ Instantiate a square
+        """
+        try:
+            super().__init__(size, size)
+        except TypeError:
+            raise TypeError("size must be an integer")
+        except ValueError:
+            raise ValueError("size must be greater than 0")
+        self.__size = size
+                                                    
