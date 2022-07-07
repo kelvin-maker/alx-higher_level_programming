3c88ddb94baf0053228f6268634779dc695fa50c
Kelvin Mwenda <nkmke1964@gmail.com>
2022-07-06 23:41:44 -0700

    corrections

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """Provides a function to read and print a file"""
+
+
 def read_file(filename=""):
     """Print the contents of a file"""
     with open(filename, "r") as istream:
         print(istream.read(), end="")
-    
+        
