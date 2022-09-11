5b16f33ced06e7cb110c7294ff2a871214823022
kelvin-maker <nkmke64@gmail.com>
2022-09-11 07:27:21 -0700

    SQL alchemist

--- 0x0F-python-object_relational_mapping/drop.sql
+++ 0x0F-python-object_relational_mapping/drop.sql
@@ -1,1 +1,1 @@
-DROP DATABASE IF EXISTS hbtn_0e_0_usa; 
+DROP DATABASE IF EXISTS hbtn_0e_4_usa; 
---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+class base and base an instance of declarative_base()
+"""
+from sqlalchemy import Column, Integer, String, MetaData
+from sqlalchemy.ext.declarative import declarative_base
+
+mymetadata = MetaData()
+Base = declarative_base(metadata=mymetadata)
+
+class State(Base):
+    """
+    class with attributes id and name form states
+    """
+
+    __tablename__ = 'states'
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
+    
